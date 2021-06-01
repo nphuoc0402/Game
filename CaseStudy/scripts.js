9a73ada4a6ec452af04fe12371e857a071f62920
Phuoc Nguyen <nphuoc0402@gmail.com>
2021-06-01 14:37:59 +0700

    change file

--- CaseStudy/scripts.js
+++ CaseStudy/scripts.js
@@ -1,6 +1,9 @@
 let container = document.getElementById('container') ;
 let player2 = document.getElementById("player2");
 let canvas = document.getElementById("gameCanvas");
+canvas.addEventListener("mousedown", handleMouseClick);
+let framePerSecond = 30;
+let game = false;
 let check = false;
 let canvasContext;
 let ballX = 400;
@@ -15,26 +18,31 @@ const PADDLE_HEIGHT = 100;
 const PADDLE_THINKNESS = 10;
 let Player1Score = 0;
 let Player2Score = 0;
-const WIN_SCORE = 10;
+const WIN_SCORE = 3;
 let showingWinScreen = false;
 function handleMouseClick() {
-  container.style.display = 'block';
+  if(!game){container.style.display = 'block';
   canvas.style.display = "none";
   player2.style.display = "none";
-
+  }
 }
+
 function startGame() {
+
   check =false;
+  game =true;
   canvas.style.display = "block";
   container.style.display = 'none';
   canvasContext = canvas.getContext("2d");
-  let framePerSecond = 30;
-  setInterval(callBoth, 1000 / framePerSecond);
-  canvas.addEventListener("mousedown", handleMouseClick);
+  setInterval(callBoth, 1000 / framePerSecond);// remove setinterval sau mỗi lần chơi
   canvas.addEventListener("mousemove", function (evt) {
     x = evt.clientX;
     y = evt.clientY;
-    paddle1Y = y - PADDLE_HEIGHT / 2;
+    if(y>=0 &&  y<= canvas.height - PADDLE_HEIGHT){
+      paddle1Y = y;
+    }
+    // console.log(paddle1Y,y);
+   
   });
   if (showingWinScreen) {
     Player1Score = 0;
@@ -48,10 +56,9 @@ function startGame1(){
   container.style.display = 'none';
   canvasContext = canvas.getContext("2d");
   player2.style.display = "block";
-  let framePerSecond = 30;
+  game =true;
   check = true;
   setInterval(callBoth, 1000 / framePerSecond);
-  canvas.addEventListener("mousedown", handleMouseClick);
   window.addEventListener("keydown", myFunction);
   window.addEventListener("keydown", myFunction1);
   if (showingWinScreen) {
@@ -81,43 +88,45 @@ function myFunction1(evt) {
   
 }
 function moveup1() {
-  if(paddle1Y < PADDLE_HEIGHT/2){
-    paddle1Y = -PADDLE_HEIGHT/2;
+  if(paddle1Y <= 0){
+    paddle1Y = 0;
   }else {
     paddle1Y -= 15;
   }
 }
 
 function movedown1() {
-  if(paddle1Y >= canvas.height - PADDLE_HEIGHT/2){
-    paddle1Y = canvas.height - PADDLE_HEIGHT/2;
+  if(paddle1Y >= canvas.height - PADDLE_HEIGHT){
+    paddle1Y = canvas.height - PADDLE_HEIGHT;
   }else {
     paddle1Y += 15;
   }
 }
 function moveup() {
-  if(paddle2Y < PADDLE_HEIGHT/2){
-    paddle2Y = -PADDLE_HEIGHT/2;
+  if(paddle2Y <= 0){
+    paddle2Y = 0;
   }else {
     paddle2Y -= 15;
   }
 }
 
 function movedown() {
-  if(paddle2Y >= canvas.height - PADDLE_HEIGHT/2){
-    paddle2Y = canvas.height - PADDLE_HEIGHT/2;
+  if(paddle2Y >= canvas.height - PADDLE_HEIGHT){
+    paddle2Y = canvas.height - PADDLE_HEIGHT;
   }else {
     paddle2Y += 15;
   }
 }
 
 function ballreset() {
+  console.log("resetball");
   if (Player1Score >= WIN_SCORE || Player2Score >= WIN_SCORE) {
     showingWinScreen = true;
+    game = false;
   }
   ballX = canvas.width / 2;
   ballY = canvas.height / 2;
-  ballSpeedX = -ballSpeedX;
+  ballSpeedX = -defaultX;
  
 }
 
@@ -126,12 +135,16 @@ function callBoth() {
   drawEverything();
 }
 function computerMovement() {
-  let paddle2YCenter = paddle2Y + PADDLE_HEIGHT / 2;
+  let paddle2YCenter = paddle2Y + PADDLE_HEIGHT/2;
   if(!check){
     if (paddle2YCenter < ballY - 25) {
-      paddle2Y = paddle2Y + 6;
+      if(paddle2Y <= canvas.height -PADDLE_HEIGHT){
+        paddle2Y = paddle2Y + 6;
+      }
     } else if (paddle2YCenter > ballY + 25) {
-      paddle2Y = paddle2Y - 6;
+      if(paddle2Y>=0){
+        paddle2Y = paddle2Y - 6;
+      }
     }
   }
   
@@ -140,34 +153,40 @@ function moveEverything() {
   computerMovement();
   ballX += ballSpeedX;
   ballY += ballSpeedY;
-  if (ballX > canvas.width-PADDLE_THINKNESS) {
+  console.log( 'ballX :'+ ballX);
+  console.log('ballY :'+ ballY);
+  if (ballX >= canvas.width-PADDLE_THINKNESS-13) {
+
     // ballSpeedX = -ballSpeedX;
     if (ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT) {
       ballSpeedX = -ballSpeedX;
       let deltaY = ballY - (paddle2Y + PADDLE_HEIGHT / 2);
-      
-      ballSpeedY = deltaY * 0.05;
-      
+      ballSpeedY = deltaY * 0.35;
     } else {
+
       Player1Score++;
+    
       ballreset();
+    
     }
   }
-  if (ballX < PADDLE_THINKNESS) {
+  if (ballX <=  PADDLE_THINKNESS+13) {
     // ballSpeedX = -ballSpeedX;
     if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
       ballSpeedX = -ballSpeedX;
       let deltaY = ballY - (paddle1Y + PADDLE_HEIGHT / 2);
-      ballSpeedY = deltaY * 0.05;
+      ballSpeedY = deltaY * 0.35;
     } else {
       Player2Score++;
+      reset();
       ballreset();
+    
     }
   }
-  if (ballY > canvas.height) {
+  if (ballY >= canvas.height-10) {
     ballSpeedY = -ballSpeedY;
   }
-  if (ballY < 0) {
+  if (ballY <= 10) {
     ballSpeedY = -ballSpeedY;
   }
 }
@@ -180,14 +199,14 @@ function drawNet() {
 function drawEverything() {
   //create the screen
   createRect(0, 0, canvas.width, canvas.height, "black");
-  //create left player paddle
-  // console.log(ballSpeedX,ballSpeedY);
+
   if (showingWinScreen) {
     canvasContext.fillStyle = "red";
     canvasContext.font = "40px Arial";
     canvasContext.textAlign = "center";
 
-    if (Player1Score >= WIN_SCORE) {      
+    if (Player1Score >= WIN_SCORE) {     
+ 
       canvasContext.fillText(
         "Player1 Win!",
         canvas.width / 2,
@@ -203,7 +222,8 @@ function drawEverything() {
       return;
 
     }
-     else if (Player2Score >= WIN_SCORE) {      
+     else if (Player2Score >= WIN_SCORE) {   
+    
       canvasContext.fillText(
         "Player2 Win!",
         canvas.width / 2,
@@ -248,3 +268,4 @@ function createRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.fillRect(leftX, topY, width, height);
 }
+/*=============================================================================*/
